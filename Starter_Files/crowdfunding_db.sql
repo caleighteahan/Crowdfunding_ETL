5896d2a4c5c85562626ba7232996f3ff8626d678
caleighteahan <caleighteahan@gmail.com>
2023-07-13 20:46:51 -0700

    keys

--- Starter_Files/crowdfunding_db.sql
+++ Starter_Files/crowdfunding_db.sql
@@ -3,13 +3,14 @@ CREATE TABLE contact_info (
     first_name VARCHAR(100),
 	last_name VARCHAR(100),
     email VARCHAR(100)
+	foreign key (contact_id) references campaign (contact_id)
 );
 Select *
 FROM contact_info
 
 CREATE TABLE campaign (
     cf_id INT,
-    contact_id INT,
+    contact_id INT PRIMARY KEY,
     company_name VARCHAR(255),
 	description VARCHAR(255),
     goal decimal,
@@ -22,19 +23,21 @@ CREATE TABLE campaign (
     end_date date,
     category_ID VARCHAR (255),
 	subcategory_ID VARCHAR(255)
+	foreign key (category_id) references categories (category_id)
+	foreign key (subcategory_id) references subcategories (subcategory_id)
 );
 select *
 from campaign
 
 CREATE TABLE subcategories (
-    subcategory_id VARCHAR(10),
+    subcategory_id VARCHAR(10)PRIMARY KEY,
     subcategory VARCHAR(255)
 );
 Select *
 FROM subcategories
 
 CREATE TABLE categories (
-    category_id VARCHAR(10),
+    category_id VARCHAR(10) PRIMARY,
     category VARCHAR(255)
 );
 SELECT *
